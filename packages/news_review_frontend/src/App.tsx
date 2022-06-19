5eb0dcb667b501581d913e36bd754075703cd98f
scott <scottberry91@gmail.com>
2022-06-19 18:05:30 +0100

    latest changes

--- packages/news_review_frontend/src/App.tsx
+++ packages/news_review_frontend/src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { useBinaryQueries } from '@thelasthurrah/authentication_api'
 
 import { Header } from './partials/Header'
 import { SiteRouter } from './router'
 
 function App() {
+	const client = useBinaryQueries()
+
+	useLayoutEffect(() => {
+		const fetchUser = async () => {
+			const result = await client.currentUser()
+			console.log('RESULT', result)
+		}
+
+		fetchUser().catch(console.error)
+	}, [])
+
 	return (
 		<div className="App">
 			<Header />
