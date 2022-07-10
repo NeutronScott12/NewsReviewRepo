2357b85525a0be49fcf8f94e546b335570dbf618
scott <scottberry91@gmail.com>
2022-07-10 15:22:28 +0100

    latest changes

--- packages/news_review_frontend/src/index.tsx
+++ packages/news_review_frontend/src/index.tsx
@@ -15,9 +15,6 @@ import { apollo_client } from './apollo'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { CommentAPI } from '@thelasthurrah/comment_api'
-// import { CommentAPI } from './demo/src'
-// import { CommentAPI } from '@thelasthurrah/comment_api'
-// import { CommentAPI } from './demo/src'
 
 const root = createRoot(document.getElementById('root')!)
 
@@ -30,8 +27,6 @@ const comment_client = new CommentAPI({
 	http_uri: 'http://localhost:4000/graphql',
 	web_socket_uri: 'ws://localhost:4003/graphql',
 	application_short_name: 'first-application',
-	apollo: true,
-	graphql_request: true,
 })
 
 //Apollo returns undefined here
@@ -39,17 +34,6 @@ comment_client.queries
 	.fetch_comemnts()
 	.then((response) => console.log('START', response))
 	.catch((err) => console.log('BIG ERROR', err))
-
-// const binaryStashClient = new BinaryStashClient({
-// 	http_uri: 'http://localhost:4000/graphql',
-// 	ws_uri: 'ws://localhost:4003/graphql',
-// 	application_short_name: 'first-application',
-// })
-
-// binaryStashClient.comment_queries
-// 	.fetch_comemnts()
-// 	.then((resp) => console.log('BINARY STASH RESPONSE', resp))
-// 	.catch(console.error)
 
 root.render(
 	<React.StrictMode>
